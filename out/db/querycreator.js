b3e47a6c1193452be9b24720f95ca1e5158a1c3f
bruceth <sunbaichuan@hotmail.com>
2019-10-24 22:24:13 +0800

    divident

--- out/db/querycreator.js
+++ out/db/querycreator.js
@@ -12,12 +12,14 @@ const queryTemplates = {
             let blackID = query.blackID === undefined || query.blackID === null ? 0 : query.blackID;
             if (blackID > 0) {
                 return `SELECT ta.order, \`id\` AS \`id\`,t0.\`symbol\` AS \`symbol\`,t0.\`market\` AS \`market\`,t0.\`code\` AS \`code\`,
-    t0.\`name\` AS \`name\`, t3.价格 as price, t3.复权 / t1.earning as \`pe\`, t1.earning as \`e\`, t2.roe as \`roe\`, t3.复权 as priceEx
+    t0.\`name\` AS \`name\`, t3.价格 as price, t3.复权 / t1.earning as \`pe\`, t1.earning as \`e\`, t2.roe as \`roe\`,
+    t3.复权 as priceEx, t5.bonus / t3.复权 as divyield
     FROM \`t_stocksorderbyuser\` as ta inner join \`tv_股票\` AS t0 on ta.user='${query.user}' and ta.stock = t0.id 
     left join l_earning as t1 on t0.id=t1.stock and t1.yearlen = '${query.yearlen}'
     left join l_roe as t2 on t0.id=t2.stock 
     left join t_股票价格复权 as t3 on t0.id= t3.stock
     left join mi.tv_tagstock as t4 on ta.user=t4.user and t4.tag='${blackID}' and ta.stock=t4.stock
+    left join t_最近年分红 as t5 on t0.id=t5.stock
     WHERE ta.order > ${query.pageStart} and t4.stock is null
     ORDER BY ta.order ASC
     LIMIT ${query.pageSize};
@@ -25,11 +27,12 @@ const queryTemplates = {
             }
             else {
                 return `SELECT ta.order, \`id\` AS \`id\`,t0.\`symbol\` AS \`symbol\`,t0.\`market\` AS \`market\`,t0.\`code\` AS \`code\`,
-    t0.\`name\` AS \`name\`, t3.价格 as price, t3.复权 / t1.earning as \`pe\`, t1.earning as \`e\`, t2.roe as \`roe\`
+    t0.\`name\` AS \`name\`, t3.价格 as price, t3.复权 / t1.earning as \`pe\`, t1.earning as \`e\`, t2.roe as \`roe\`, t4.bonus / t3.复权 as divyield
     FROM \`t_stocksorderbyuser\` as ta inner join \`tv_股票\` AS t0 on ta.user='${query.user}' and ta.stock = t0.id 
     left join \`l_earning\` as t1 on t0.id=t1.stock and t1.yearlen = '${query.yearlen}'
     left join \`l_roe\` as t2 on t0.id=t2.stock 
     left join t_股票价格复权 as t3 on t0.id= t3.stock
+    left join t_最近年分红 as t4 on t0.id=t4.stock
     WHERE ta.order > ${query.pageStart}
     ORDER BY ta.order ASC
     LIMIT ${query.pageSize};
@@ -60,11 +63,12 @@ insert into t_stocksorderbyuser (\`user\`, \`order\`, \`stock\`) select '${query
         },
         sql: (query, params) => {
             return `SELECT ta.order, \`id\` AS \`id\`,t0.\`symbol\` AS \`symbol\`,t0.\`market\` AS \`market\`,t0.\`code\` AS \`code\`,
-    t0.\`name\` AS \`name\`, t3.价格 as price, t3.复权 / t1.earning as \`pe\`, t1.earning as \`e\`, t2.roe as \`roe\`
+    t0.\`name\` AS \`name\`, t3.价格 as price, t3.复权 / t1.earning as \`pe\`, t1.earning as \`e\`, t2.roe as \`roe\`, t4.bonus / t3.复权 as divyield
     FROM \`t_stocksorderbyusertag\` as ta inner join \`tv_股票\` AS t0 on ta.user='${query.user}' and ta.stock = t0.id 
     left join \`l_earning\` as t1 on t0.id=t1.stock and t1.yearlen = '${query.yearlen}'
     left join \`l_roe\` as t2 on t0.id=t2.stock 
     left join t_股票价格复权 as t3 on t0.id= t3.stock
+    left join t_最近年分红 as t4 on t0.id=t4.stock
     WHERE ta.order > ${query.pageStart}
     ORDER BY ta.order ASC
     LIMIT ${query.pageSize};
