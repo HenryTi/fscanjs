39d9101ef49884e084a360a1f31274413a805ac6
bruceth <sunbaichuan@hotmail.com>
2019-10-26 22:04:40 +0800

    sort

--- src/db/querycreator.ts
+++ src/db/querycreator.ts
@@ -146,6 +146,42 @@ insert into t_stocksorderbyuser (\`user\`, \`order\`, \`stock\`) select '${query
 ` 
     },
   },
+  tagdp: {
+    check: (query: any, params: any[]) => {
+      return query.user !== undefined && query.user !== null &&
+            query.yearlen !== undefined && query.yearlen !== null &&
+            query.tag !== undefined && query.tag !== null &&
+            query.pageStart !== undefined && query.pageStart !== null &&
+            query.pageSize !== undefined && query.pageStart !== null 
+    },
+    sql: (query: any, params: any[]) => { 
+      return `SELECT ta.order, \`id\` AS \`id\`,t0.\`symbol\` AS \`symbol\`,t0.\`market\` AS \`market\`,t0.\`code\` AS \`code\`,
+    t0.\`name\` AS \`name\`, t3.价格 as price, t3.复权 / t1.earning as \`pe\`, t1.earning as \`e\`, t2.roe as \`roe\`, t4.bonus / t3.复权 as divyield
+    FROM \`t_stocksorderbyusertag\` as ta inner join \`tv_股票\` AS t0 on ta.user='${query.user}' and ta.stock = t0.id 
+    left join \`l_earning\` as t1 on t0.id=t1.stock and t1.yearlen = '${query.yearlen}'
+    left join \`l_roe\` as t2 on t0.id=t2.stock 
+    left join t_股票价格复权 as t3 on t0.id= t3.stock
+    left join t_最近年分红 as t4 on t0.id=t4.stock
+    WHERE ta.order > ${query.pageStart}
+    ORDER BY ta.order ASC
+    LIMIT ${query.pageSize};
+` 
+    },
+    createsql: (query: any, parasm: any[]) => {
+      let user = query.user as number;
+      let userStr = Number.isNaN(user) ? '' : user.toString();
+      let tempTableName = '_tmporderbyusertag_' + userStr;
+      return `DROP TEMPORARY TABLE IF EXISTS \`${tempTableName}\`;
+CREATE TEMPORARY TABLE \`${tempTableName}\` (\`no\` INT NOT NULL AUTO_INCREMENT, \`stock\` INT NULL, PRIMARY KEY(\`no\`)) ENGINE=MyISAM;
+delete from t_stocksorderbyusertag where \`user\`='${query.user}';
+insert into \`${tempTableName}\` (stock) select a.stock as stock from t_股票价格复权 as a 
+  inner join mi.tv_tagstock as c on c.user='${query.user}' and c.tag=${query.tag} and c.stock=a.stock
+  left join t_最近年分红 as b on a.stock = b.stock
+  order by b.bonus / a.复权 DESC limit 2000;
+insert into t_stocksorderbyusertag (\`user\`, \`order\`, \`stock\`) select '${query.user}' as \`user\`, \`no\` as \`order\`, \`stock\` from \`${tempTableName}\`;
+` 
+    },
+  },
 }
 
 export class QueryCreator {
