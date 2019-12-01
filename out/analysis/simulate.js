96d8f612e5aa7d0b09364491c0dcb4de75775148
Henry <liaohengyi@outlook.com>
2019-12-01 11:39:59 +0800

    simulate easier

--- out/analysis/simulate.js
+++ out/analysis/simulate.js
@@ -4,29 +4,16 @@ const price_1 = require("./price");
 const reports_1 = require("./reports");
 const data_1 = require("./data");
 class Simulate {
-    //private tradeType:TradeType;
-    //private rankType:RankType;
-    //private trader: Trader;
-    //private rank: Rank;
-    constructor(//startDate:Date, endDate:Date, 
-    step, actions) {
-        //this.startDate = startDate;
-        //this.endDate = endDate;
+    constructor(step, actions) {
         this.step = step;
         this.actions = actions;
-        //this.tradeType = tradeType;
-        //this.rankType = rankType;
     }
     async run() {
         await data_1.data.init();
-        //this.trader = this.createTrader();
-        //this.rank = this.createRank();
         let prices = new price_1.Prices();
         let reports = new reports_1.Reports();
         let cash = 3000000;
         let holdings = {};
-        // 可以loadHoldings
-        //this.trader.initHoldings(cash, holdings);
         for (let action of this.actions) {
             action.trader.initHoldings(cash, holdings);
         }
--- src/analysis/simulate.ts
+++ src/analysis/simulate.ts
@@ -1,45 +1,31 @@
-import { Rank, ROERank, ROE_PE_Dividend_Rank } from "./rank";
+import { Rank } from "./rank";
 import { Prices } from "./price";
 import { Reports } from "./reports";
-import { Trader, TraderYearOverYear, Trader2X2, Trader6P1 } from "./trader";
+import { Trader } from "./trader";
 import { Holdings } from "./holding";
 import { data } from "./data";
 import { Step } from "./step";
 
 export class Simulate {
-    //private startDate:Date;
-    //private endDate:Date;
     private step:Step;
     private actions:{trader:Trader, rank:Rank}[];
-    //private tradeType:TradeType;
-    //private rankType:RankType;
-    //private trader: Trader;
-    //private rank: Rank;
 
-    constructor(//startDate:Date, endDate:Date, 
+    constructor(
         step: Step,
         actions: {trader:Trader, rank:Rank}[])
     {
-        //this.startDate = startDate;
-        //this.endDate = endDate;
         this.step = step;
         this.actions = actions;
-        //this.tradeType = tradeType;
-        //this.rankType = rankType;
     }
 
     async run():Promise<void> {
         await data.init();
 
-        //this.trader = this.createTrader();
-        //this.rank = this.createRank();
         let prices = new Prices();
         let reports = new Reports();
 
         let cash = 3000000;
         let holdings: Holdings = {};
-        // 可以loadHoldings
-        //this.trader.initHoldings(cash, holdings);
         for (let action of this.actions) {
             action.trader.initHoldings(cash, holdings);
         }
@@ -58,23 +44,4 @@ export class Simulate {
             }
         }
     }
-
-    /*
-    private createTrader():Trader {
-        switch (this.tradeType) {
-            default: throw new Error(`Trade type ${this.tradeType} not defined`);
-            case 'tradeYearOverYear': return new TraderYearOverYear();
-            case 'trade2X2': return new Trader2X2();
-            case 'trade6P1': return new Trader6P1();
-        }
-    }
-
-    private createRank():Rank {
-        switch (this.rankType) {
-            default: throw new Error(`Rank type ${this.rankType} not defined`);
-            case 'ROE': return new ROERank();
-            case 'ROE_PE_Dividend': return new ROE_PE_Dividend_Rank();
-        }
-    }
-    */
 }
